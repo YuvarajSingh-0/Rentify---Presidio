65cffefcc93309ee933eacc9939824eb72009311
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2024-05-26 20:02:50 +0530

    Updates

---
+++ static/js/289.efe22603.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront=self.webpackChunkfront||[]).push([[289],{289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.efe22603.chunk.js.map
